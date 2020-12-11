7c8f6213edd637181714139c4a9954e3a4c906a0
Fatih <fatih__75@hotmail.com>
2020-12-12 01:07:52 +0300

    Working version

--- Spectrogram.py
+++ Spectrogram.py
@@ -2,9 +2,9 @@ import numpy as np
 import soundfile as sf
 from scipy import signal
 import scipy
-from scipy.fft import fft
 import matplotlib.pyplot as plt
-
+from scipy import fft
+import time
 class BaseSpectorgram():
 
     def __init__(self,args):
@@ -15,6 +15,12 @@ class BaseSpectorgram():
             self.overlapLen =self.windowLen-self.shiftLen
             self.nomFreq = args['nomFreq']
             self.minSampFreq = self.nomFreq * 3
+            self.isZeropadding=args['zeroPadding']
+            self.isOneSided=args['oneSided']
+            self.p2=args['p2']
+            self.windowType=args['windowType']
+            self.interpolMethod=args['interpolMethod']
+
 
             self.audioSamples = None
             self.fsOriginal = None
@@ -24,6 +30,7 @@ class BaseSpectorgram():
             self.timeAxis = None
             self.freqResponse = None
             self.psd=None
+            self.binCount=None
             self.ReadAudio()
 
     def ReadAudio(self):
@@ -45,6 +52,7 @@ class BaseSpectorgram():
         self.windowSize=int(np.floor(self.windowLen*self.fsNew))
         self.shiftSize=int(np.floor(self.shiftLen * self.fsNew))
         self.overlapSize = self.windowSize-self.shiftSize
+        self.window = signal.get_window(self.windowType, self.windowSize)
 
     def GetSamplingOrjFreq(self):
         return self.fsOriginal
@@ -52,69 +60,166 @@ class BaseSpectorgram():
     def GetAudioData(self):
         return self.audioSamples
 
-    def ManualSpectogram(self):
+    def ManualSpectogramWithStride(self):
+
+        shp = ((self.decimatedSignal.shape[0]-self.overlapSize) // self.shiftSize,self.windowSize)
+        strides = (self.shiftSize*self.decimatedSignal.strides[0],self.decimatedSignal.strides[0])
+        sample = np.lib.stride_tricks.as_strided(self.decimatedSignal, shape=shp, strides=strides)
+
+        sample = signal.detrend(sample, type='constant', axis=-1)
+        sample *= self.window
+        self.binCount = 2 ** self.p2 if self.isZeropadding else self.windowSize
+
+        if self.isOneSided:
+            result=fft.rfft(sample.real, n=self.binCount)
+        else:
+            result = fft(sample, self.binCount)
+
+
+        scale = 1.0 / (self.fsNew * (self.window * self.window).sum())
+        self.freqResponse=(result * np.sqrt(scale))
+
+        dFs = self.fsNew / self.binCount
+
+        if self.isOneSided:
+
+           rng=np.arange(self.freqResponse.shape[1])
+           self.freqAxis=rng*dFs
+           self.psd = np.conjugate(self.freqResponse) * self.freqResponse
+
+           if self.binCount%2:
+               self.psd[...,1:]*=2
+           else:
+               self.psd[...,1:-1]*=2
+
+        else:
+            rng1 = np.arange(int(self.freqResponse.shape[1] / 2))
+            rng2 = -np.arange(int(self.freqResponse.shape[1] / 2), 0, -1)
+            self.freqAxis = np.hstack((rng1 * dFs, rng2 * dFs))
+            self.psd = np.conjugate(self.freqResponse) * self.freqResponse
+
+        self.timeAxis = np.arange(self.windowSize / 2, self.decimatedSignal.shape[0] - self.windowSize / 2, self.shiftSize) / self.fsNew
+
+        return self.freqAxis,self.timeAxis,self.freqResponse.T,self.psd.real.T
+
+
+
+    def ManualSpectogramWithLoop(self):
+
         i = 0
-        spec = []
         freqRes=[]
         Nx = self.decimatedSignal.shape[0]
+        self.binCount = 2 ** self.p2 if self.isZeropadding else self.windowSize
 
         while True:
-
             rng = range(self.shiftSize * i, self.windowSize + self.shiftSize * i)
             if self.windowSize + self.shiftSize * i >= Nx:
                 break
+
             i += 1
             sample = self.decimatedSignal[rng]
             sample = signal.detrend(sample, type='constant', axis=-1)
-            sample *= np.hamming(sample.shape[0])
-            sample = sample.real
-            result = scipy.fft.rfft(sample, n=self.windowSize)
-            freqRes.append(result)
+            sample *= self.window
 
-            ##Calculate power spectrum density(psd)
-            win = signal.get_window('hamming', self.windowSize)
-            scale = 1.0 / (self.fsNew * (win * win).sum())
-            result = np.conjugate(result) * result
-            result *= scale
-
-            if self.windowSize % 2:
-                result[..., 1:] *= 2
+            if self.isOneSided:
+                result = fft.rfft(sample.real, n=self.binCount)
             else:
-                # Last point is unpaired Nyquist freq point, don't double
-                result[..., 1:-1] *= 2
+                result = fft(sample, self.binCount)
 
-            spec.append(result.real)
+            scale = 1.0 / (self.fsNew * (self.window * self.window).sum())
+            freqRes.append(result*np.sqrt(scale))
 
-            # step = nperseg - noverlap
-            # shape = x.shape[:-1] + ((x.shape[-1] - noverlap) // step, nperseg)
-            # strides = x.strides[:-1] + (step * x.strides[-1], x.strides[-1])
-            # result = np.lib.stride_tricks.as_strided(x, shape=shape,
-            #                                          strides=strides)
+        self.freqResponse=np.vstack(freqRes)
+        dFs = self.fsNew / self.binCount
 
-        self.psd = np.vstack(spec).T
-        self.freqResponse=np.vstack(freqRes).T
+        if self.isOneSided:
+           rng=np.arange(self.freqResponse.shape[1])
+           self.freqAxis=rng*dFs
+           self.psd = np.conjugate(self.freqResponse) * self.freqResponse
 
-        dFs=self.fsNew/self.windowSize
-        rng=np.arange(self.freqResponse.shape[0])
-        self.freqAxis=rng*dFs
+           if self.binCount%2:
+
+               self.psd[...,1:]*=2
+           else:
+               self.psd[...,1:-1]*=2
+
+        else:
+            rng1 = np.arange(int(self.freqResponse.shape[1] / 2))
+            rng2 = -np.arange(int(self.freqResponse.shape[1] / 2), 0, -1)
+            self.freqAxis = np.hstack((rng1 * dFs, rng2 * dFs))
+            self.psd = np.conjugate(self.freqResponse) * self.freqResponse
+
         self.timeAxis=np.arange(self.windowSize/2,Nx-self.windowSize/2,self.shiftSize)/self.fsNew
 
-        return self.freqAxis,self.timeAxis,self.psd,self.freqResponse
+        return self.freqAxis,self.timeAxis,self.freqResponse.T,self.psd.real.T
 
     def SpectogramWithScipy(self):
-        self.freqAxis, self.timeAxis, self.psd = signal.spectrogram(self.decimatedSignal, self.fsNew,
-                                                                             window=signal.get_window('hamming',
-                                                                                                      self.windowSize),
-                                                                             noverlap=self.overlapSize, mode='psd')
-        return self.freqAxis, self.timeAxis, self.psd
+
+        self.binCount = 2 ** self.p2 if self.isZeropadding else self.windowSize
+        self.freqAxis, self.timeAxis, self.freqResponse = signal.spectrogram(self.decimatedSignal, self.fsNew
+                                                                             ,nfft=self.binCount,
+                                                                             window=self.window,
+                                                                             noverlap=self.overlapSize, mode='complex',return_onesided=self.isOneSided)
+        self.psd = np.conjugate(self.freqResponse.T) * (self.freqResponse.T)
+
+        if self.isOneSided:
+            if self.binCount % 2:
+                self.psd[..., 1:] *= 2
+            else:
+                self.psd[..., 1:-1] *= 2
+
+        return self.freqAxis, self.timeAxis, self.freqResponse,self.psd.real.T
 
     def PlotSpectogram(self,timeAxis,freqAxis,psd,plotName):
-        plt.pcolormesh(timeAxis, freqAxis, psd, shading='gouraud')
+        if self.isOneSided:
+            plt.pcolormesh(timeAxis, freqAxis, psd, shading='gouraud')
+        else:
+            plt.pcolormesh(timeAxis, fft.fftshift(freqAxis),fft.fftshift(psd, axes=0), shading='gouraud')
         plt.ylabel('Frequency [Hz]')
         plt.xlabel('Time [sec]')
         plt.title(plotName)
         plt.show()
 
-    # def PlotNominalFreq(self):
+    def CalculateNominalFreq(self,inputMat):
+        maxIndices=np.argmax(inputMat,axis=0)
+        maxFreqs=self.freqAxis[maxIndices]
+        return maxFreqs
+
+    def PlotNominalFreq(self, freqs):
+        plt.plot(freqs)
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+        plt.show()
+    def Interpolater(self,inputMat):
+
+        inputMat=inputMat.T
+        maxIdx=np.argmax(inputMat,axis=1)
+        idx=np.arange(inputMat.shape[0])[:,np.newaxis]
+        X=np.vstack((maxIdx-1,maxIdx,maxIdx+1))
+        Y=inputMat[idx,X.T]
+        D01=X[0]-X[1]
+        D02=X[0]-X[2]
+        D12=X[1]-X[2]
+
+        if self.interpolMethod=='Polynomial':
+            Y = Y.T
+            A1 = (Y[1] - Y[0]) / -D01
+            A2 = (-D02) * ((Y[2] - Y[1]) / -D12 - (Y[1] - Y[0]) / -D01)
+            Fmax=0.5*(X[1]+X[2]-A1/A2)*self.fsNew / self.binCount
+
+        elif self.interpolMethod=='Lagrange':
+            D=np.vstack((D01*D02, -D01*D12, -D02*-D12)).T
+            A=Y/D
+            S=np.vstack((X[1]+X[2], X[0]+X[2], X[0]+X[1])).T
+            S=-np.sum(A*S,axis=1)
+            Fmax=-S/(2*np.sum(A,axis=1))*self.fsNew / self.binCount
+        else:
+            raise Exception("Interpolation method can be selected as Polynomial or Lagrange only")
+
+        return Fmax
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 from Spectrogram import BaseSpectorgram
 import matplotlib.pyplot as plt
+import time
 def str2bool(value):
     return value == "True"
 
@@ -10,17 +11,33 @@ if __name__=="__main__":
     argsParser.add_argument("--sourcePath", default="./data/newtest.wav", help="the location of audio source")
     ##STFT related arguments
     argsParser.add_argument("--windowLen", type=float,default=20, help="length of STFT window in second")
-    argsParser.add_argument("--windowShift", type=float, default=1, help="length of window shift in second")
+    argsParser.add_argument("--windowShift", type=float, default=19, help="length of window shift in second")
     argsParser.add_argument("--nomFreq", type=float, default=50, help="nominal frequency of input signal")
-    argsParser.add_argument("--zeroPadding", type=str2bool, default=True, help="if true, calculate fft for 2^powerOf2 points")
-    argsParser.add_argument("--p2", type=int, default=16, help="power of two to be used in fft zero-padding")
-
+    argsParser.add_argument("--zeroPadding", type=str2bool, default=True, help="if true, calculate fft for 2^p2 points")
+    argsParser.add_argument("--p2", type=int, default=18, help="power of two to be used in fft zero-padding")
+    argsParser.add_argument("--oneSided", type=str2bool, default=True, help="If true, calculate one-sided fft")
+    argsParser.add_argument("--windowType", default='hamming', help="window type for stft calculation")
+    argsParser.add_argument("--interpolMethod", default='Lagrange', help="Interpolation method can be set to Polynomial or Lagrange ")
     args = vars(argsParser.parse_args())
     spec=BaseSpectorgram(args)
 
-    freqAxis,timeAxis,ss=spec.SpectogramWithScipy()
-    # spec.PlotSpectogram(timeAxis,freqAxis,ss,"Scipy spec")
-    fr,TM,psd,freq=spec.ManualSpectogram()
-    spec.PlotSpectogram(TM, fr, psd,"My Spec")
-    freq=freq
+    # c1=time.time()
+    # freqAxis1,timeAxis1,freqRes1,psd1=spec.ManualSpectogramWithStride()
+    # print("Time for stride",time.time()-c1)
+    #
+    # c2=time.time()
+    # freqAxis2,timeAxis2,freqRes2,psd2=spec.SpectogramWithScipy()
+    # print("Time for scipy",time.time()-c2)
+    # spec.PlotSpectogram(timeAxis2, freqAxis2, psd2,"My Spec")
+    c3=time.time()
+    freqAxis3,timeAxis3,freqRes3,psd3=spec.ManualSpectogramWithLoop()
+    print("Time for loop",time.time()-c3)
+    freqs=spec.Interpolater(psd3)
+    freqs1=spec.CalculateNominalFreq(psd3)
+    # freqs2 = spec.CalculateNominalFreq(psd2)
+    # freqs33 = spec.CalculateNominalFreq(psd3)
+    spec.PlotNominalFreq(freqs1)
+    # spec.PlotNominalFreq(freqs)
+    # spec.PlotSpectogram(TM, fr, psd2,"My Spec")
+
 
